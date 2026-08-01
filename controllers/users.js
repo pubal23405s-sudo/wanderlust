292bcbaf27568c4c49166e2d3b81b729df6d7738
Pubal-X <pubal23405s@gmail.com>
2026-08-02 00:07:46 +0530

    Add Project Files

---
+++ controllers/users.js
@@ -0,0 +1,44 @@
+const User = require("../models/user.js");
+
+module.exports.renderSignupForm = (req, res) => {
+    res.render("users/signup");
+}
+
+module.exports.signup = async (req, res) => {
+    try {
+        let { username, email, password } = req.body;
+        const newUser = new User({ email, username });
+        const registerUser = await User.register(newUser, password);
+        console.log(registerUser);
+        req.login(registerUser, (err) => {
+            if (err) {
+                return next(err);
+            }
+            req.flash("success", "Welcome to Wanderlust");
+            res.redirect("/listings");
+        });
+    } catch (e) {
+        req.flash("error", e.message);
+        res.redirect("/signup");
+    }
+}
+
+module.exports.renderLoginForm = (req, res) => {
+    res.render("users/login");
+}
+
+module.exports.login = (req, res) => {
+    req.flash("success", "Welcome to Wanderlust!");
+    let redirectUrl = res.locals.redirectUrl || "/listings";
+    res.redirect(redirectUrl);
+}
+
+module.exports.logout = (req, res, next) => {
+    req.logout((err) => {
+        if (err) {
+            return next(err);
+        }
+        req.flash("success", "Logged out successfully");
+        res.redirect("/listings");
+    });
+}
